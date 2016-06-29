d41b8b30d4ff8965d882b211972cec1563ed2a87
Erik Svedäng <erik.svedang@gmail.com>
2016-06-29 14:27:54 +0200

    switch to Data.Map.Strict complete

--- Main.hs
+++ Main.hs
@@ -8,22 +8,15 @@ import Data.List
 import qualified Data.Map.Strict as Map
 
 type Freq = Int
-type Weights = [(Char, Freq)]
+type Weights = Map.Map Char Freq
 type Mapping = Map.Map Char Weights
 
-addToAL :: Eq key => [(key, elt)] -> key -> elt -> [(key, elt)]
-addToAL l key value = (key, value) : delFromAL l key
-
-delFromAL :: Eq key => [(key, a)] -> key -> [(key, a)]
-delFromAL l key = filter (\a -> (fst a) /= key) l
-
 -- | Sums up the frequency count of a list of weights.
--- | i.e. totalFreq [('a',10),('b',7)] => 17
-totalFreq :: [(t, Int)] -> Int
-totalFreq weights = foldl' (\total (_, x) -> total + x) 0 weights
+totalFreq :: Weights -> Int
+totalFreq weights = Map.foldl' (\total x -> total + x) 0 weights
 
 -- | Given a list of frequency weigths, find the weight at the position between 0 and the total frequency count.
--- | i.e. findSlice [('a',10),('b',7)] 12 => Just 'b'
+findSlice :: Weights -> Int -> Maybe Char
 findSlice weights n = 
   let finder (totalIndex, result) weight = case result of
         Just _ -> (totalIndex, result)
@@ -31,7 +24,7 @@ findSlice weights n =
                        totalIndex' = totalIndex + freq
                        result' = if totalIndex' >= n then (Just c) else Nothing
                    in (totalIndex', result')
-  in snd $ foldl' finder (0 :: Int, Nothing) weights
+  in snd $ foldl' finder (0 :: Int, Nothing) (Map.toList weights)
 
 nextChar :: Mapping -> Char -> State StdGen Char
 nextChar mapping currentChar =
@@ -57,15 +50,16 @@ chain mappings start n =
 
 increaseFreq :: Weights -> Char -> Weights
 increaseFreq weights c =
-  addToAL weights c $ case lookup c weights of
-                        (Just freq) -> freq + 1
-                        Nothing -> 1
+  let x = case Map.lookup c weights of
+                 (Just freq) -> freq + 1
+                 Nothing -> 1
+  in Map.insert c x weights
 
 visit :: Mapping -> (Char, Char) -> Mapping
 visit mappings (c, next) =
   case (Map.lookup c mappings) of
     (Just weights) -> Map.insert c (increaseFreq weights next) mappings
-    Nothing -> Map.insert c [(next, 1)] mappings
+    Nothing -> Map.insert c (Map.singleton next 1) mappings
 
 analyze :: [Char] -> Mapping
 analyze input = foldl' visit Map.empty (zip input (tail input))
